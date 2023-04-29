0822391f9d3af9c90841ce4cf3a857a6a7ac22f1
Jonatas E S Silva <jonatassilvaemanuel@gmail.com>
2023-04-28 21:39:43 -0300

    feat: user model done.

--- app/app/settings.py
+++ app/app/settings.py
@@ -16,10 +16,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+# Quick-start develpment settings - unsuitable for production
+# See https://docs.djangoproject.com/enRITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-j&i6c_5x!25uxgk^%rn7^9uaoxweltf)roj2c-%qdg2(3(2g+3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -81,7 +79,6 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
-        'PORT': os.environ.get('PORT', "5432"),
     }
 }
 
