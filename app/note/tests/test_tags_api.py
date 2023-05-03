da9ba10e1cf593a854c8b51ba5ef2ed354bf4c84
Jonatas E S Silva <jonatassilvaemanuel@gmail.com>
2023-05-03 18:08:59 -0300

    fix: test import

--- app/note/tests/test_tags_api.py
+++ app/note/tests/test_tags_api.py
@@ -5,11 +5,11 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
+from rest_framework.test import APIClient
+from rest_framework import status
+
 from note.serializers import TagSerializer
 from core.models import Tag
-
-from rest_framework import (APIClient,
-                            status)
 
 TAGS_URL = reverse('note:tag-list')
 
